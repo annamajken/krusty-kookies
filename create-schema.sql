0ae34309d6156da28cf14e8d2b87e1f7796db895
Annelie <75272048+Annelizee@users.noreply.github.com>
2021-03-30 13:21:32 +0200

    Update create-schema.sql

--- create-schema.sql
+++ create-schema.sql
@@ -1,1 +1,69 @@
--- fyll med kod
+SET FOREIGN_KEY_CHECKS = 0;
+drop table if exists Ingredients;
+drop table if exists Products;
+drop table if exists Recipes;
+drop table if exists Customers;
+drop table if exists Orders;
+drop table if exists Pallets;
+drop table if exists OrderSpecs;
+
+SET FOREIGN_KEY_CHECKS = 1;
+
+CREATE TABLE Ingredients (
+name varChar(30),
+quantity int,
+deliveryDate DATE,
+deliveredAmount int,
+unit varChar(10),
+PRIMARY KEY (name)
+);
+
+CREATE TABLE Products (
+productID int AUTO_INCREMENT,
+name varChar(30),
+price int,
+PRIMARY KEY (productID)
+);
+
+CREATE TABLE Recipes (
+productID int,
+rawMaterial varChar(30),
+amount int,
+PRIMARY KEY (productName, rawMaterial),
+FOREIGN KEY (rawMaterial) REFERENCES Ingredients (name),
+FOREIGN KEY (productID) REFERENCES Products (productID)
+);
+
+CREATE TABLE Customers (
+customerID int AUTO_INCREMENT,
+address varChar(60),
+name varChar(40),
+PRIMARY KEY (customerID),
+);
+
+CREATE TABLE Orders (
+orderNbr int AUTO_INCREMENT,
+customer int,
+PRIMARY KEY (orderNbr),
+FOREIGN KEY (customer) REFERENCES Customers (customerID)
+);
+
+CREATE TABLE Pallets (
+palletNbr int AUTO_INCREMENT,
+productID int,
+dateAndTimeOfProduction DATETIME,
+dateAndTimeOfDelivery DATETIME,
+orderNbr int,
+blocked boolean NOT NULL DEFAULT FALSE,
+PRIMARY KEY (palletNbr),
+FOREIGN KEY (productID) REFERENCES Products (productID)
+);
+
+CREATE TABLE OrderSpecs (
+orderNbr int,
+productID int,
+quantity int,
+PRIMARY KEY (orderNbr, productID),
+FOREIGN KEY orderNbr REFERENCES Orders (orderNbr),
+FOREIGN KEY productID REFERENCES Products (productID)
+);
