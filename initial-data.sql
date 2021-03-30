000b2cd1483bea8de6b82ef044c20ffc6847fe5c
Anna Bergvall <an7151be-s@student.lu.se>
2021-03-30 10:58:06 +0200

    Update initial-data.sql
    added data

---
+++ initial-data.sql
@@ -0,0 +1,31 @@
+-- Fills the tables with default data --
+
+-- Inserts data into the Recipes table --
+
+-- Almond Delight --
+insert into Recipes(productName, ingredient, amount)
+values('Almond Delight', 'Butter', 400);
+insert into Recipes(productName, ingredient, amount)
+values('Almond Delight', 'Chopped almonds', 279);
+insert into Recipes(productName, ingredient, amount)
+values('Almond Delight', 'Cinnamon', 10);
+insert into Recipes(productName, ingredient, amount)
+values('Almond Delight', 'Flour', 400);
+insert into Recipes(productName, ingredient, amount)
+values('Almond Delight', 'Sugar', 270);
+
+-- Amneris --
+insert into Recipes(productName, ingredient, amount)
+values('Amneris', 'Butter', 250);
+insert into Recipes(productName, ingredient, amount)
+values('Amneris', 'Eggs', 250);
+insert into Recipes(productName, ingredient, amount)
+values('Amneris', 'Marzipan', 750);
+insert into Recipes(productName, ingredient, amount)
+values('Amneris', 'Potato starch', 25); 
+insert into Recipes(productName, ingredient, amount)
+values('Amneris', 'Wheat flour', 25);
+
+-- Berliner --
+insert into Recipes(productName, ingredient, amount)
+values('Berliner', 'Butter', 250);
