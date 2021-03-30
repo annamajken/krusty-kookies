b33ab31cf5235a5fe10c30f6de68c573a3eba5e0
Anna Bergvall <an7151be-s@student.lu.se>
2021-03-30 11:10:19 +0200

    Update initial-data.sql
    added more data

--- initial-data.sql
+++ initial-data.sql
@@ -29,3 +29,58 @@ values('Amneris', 'Wheat flour', 25);
 -- Berliner --
 insert into Recipes(productName, ingredient, amount)
 values('Berliner', 'Butter', 250);
+insert into Recipes(productName, ingredient, amount)
+values('Berliner', 'Chocolate', 50);
+insert into Recipes(productName, ingredient, amount)
+values('Berliner', 'Eggs', 50);
+insert into Recipes(productName, ingredient, amount)
+values('Berliner', 'Flour', 350);
+insert into Recipes(productName, ingredient, amount)
+values('Berliner', 'Icing sugar', 100);
+insert into Recipes(productName, ingredient, amount)
+values('Berliner', 'Vanilla sugar', 5);
+
+-- Nut Cookie --
+
+insert into Recipes(productName, ingredient, amount)
+values('Nut Cookie', 'Bread crumbs', 125);
+insert into Recipes(productName, ingredient, amount)
+values('Nut Cookie', 'Chocolate', 50);
+insert into Recipes(productName, ingredient, amount)
+values('Nut Cookie', 'Egg whites', 350);
+insert into Recipes(productName, ingredient, amount)
+values('Nut Cookie', 'Fine-ground nuts', 750);
+insert into Recipes(productName, ingredient, amount)
+values('Nut Cookie', 'Ground, roasted nuts', 625);
+insert into Recipes(productName, ingredient, amount)
+values('Nut Cookie', 'Sugar', 375);
+
+-- Nut Ring --
+insert into Recipes(productName, ingredient, amount)
+values('Nut Ring', 'Butter', 450);
+insert into Recipes(productName, ingredient, amount)
+values('Nut Ring', 'Flour', 450);
+insert into Recipes(productName, ingredient, amount)
+values('Nut Ring', 'Roasted, chopped nuts', 225);
+
+-- Tango --
+insert into Recipes(productName, ingredient, amount)
+values('Tango', 'Butter', 200);
+insert into Recipes(productName, ingredient, amount)
+values('Tango', 'Flour', 300);
+insert into Recipes(productName, ingredient, amount)
+values('Tango', 'Sodium bicarbonate', 4);
+insert into Recipes(productName, ingredient, amount)
+values('Tango', 'Sugar', 250);
+insert into Recipes(productName, ingredient, amount)
+values('Tango', 'Vanilla', 2);
+
+
+
+
+
+
+
+
+
+
