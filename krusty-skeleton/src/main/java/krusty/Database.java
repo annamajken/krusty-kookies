89637f52f3e51cd728fb3c25905e095e4c1da3bb
Annelie <75272048+Annelizee@users.noreply.github.com>
2021-04-05 02:39:28 +0200

    Implemented the createPallet() method

--- krusty-skeleton/src/main/java/krusty/Database.java
+++ krusty-skeleton/src/main/java/krusty/Database.java
@@ -5,7 +5,10 @@ import spark.Response;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.Arrays;
 import java.util.Map;
 import java.util.TreeMap;
@@ -17,13 +20,13 @@ public class Database {
 	 * Modify it to fit your environment and then use this string when connecting to your database!
 	 */
 	private static final String jdbcString = "jdbc:mysql://localhost/krusty";
-
-	private Connection connection;
 	
 	// For use with MySQL or PostgreSQL
 	private static final String jdbcUsername = "<CHANGE ME>";
 	private static final String jdbcPassword = "<CHANGE ME>";
 
+	private Connection connection;
+	
 	public void connect() {
 		try {
 			connection = DriverManager.getConnection(jdbcString, jdbcUsername, jdbcPassword);
@@ -60,6 +63,32 @@ public class Database {
 	}
 
 	public String createPallet(Request req, Response res) {
-		return "{}";
+		String product = req.queryParams("cookie");
+		try (PreparedStatement ps = connection.prepareStatement("SELECT * FROM Products WHERE name = ?")) {
+			ps.setString(1, product);
+			ResultSet rs = ps.executeQuery();
+			if (!rs.next()) return "{ \r\n  \"status\": \"unknown cookie\" \r\n}";
+		} catch (SQLException exception) {
+			System.err.println(exception);
+			exception.printStackTrace();
+			return "{ \r\n  \"status\": \"error\" \r\n}";
+		}
+		
+		try (PreparedStatement ps = connection.prepareStatement(
+				"INSERT INTO Pallets (productID) SELECT productID FROM Products WHERE name = ?",
+				Statement.RETURN_GENERATED_KEYS)) {
+			ps.setString(1, product);
+			ResultSet rs = ps.executeQuery();
+			updateIngredients(product);
+			return "{ \r\n  \"status\": \"ok\", \r\n  \"id\": " + rs.getInt("productID") + " \r\n}}";
+		} catch (SQLException exception) {
+			System.err.println(exception);
+			exception.printStackTrace();
+			return "{ \r\n  \"status\": \"error\" \r\n}";
+		}
+	}
+	
+	private void updateIngredients(String product) {
+		
 	}
 }
