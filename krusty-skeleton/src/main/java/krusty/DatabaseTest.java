17141c088bd50d706d87accfaf264684d1b0f41b
Annelie <75272048+Annelizee@users.noreply.github.com>
2021-05-04 23:41:57 +0200

    Delete DatabaseTest.java

--- krusty-skeleton/src/main/java/krusty/DatabaseTest.java
+++
@@ -1,9 +0,0 @@
-package krusty;
-
-public class DatabaseTest {
-	public static void main(String args[]) { 
-		Database db = new Database(); 
-		db.connect(); 
-		System.out.println(db.getCustomers(null, null)); 
-	} 
-}
